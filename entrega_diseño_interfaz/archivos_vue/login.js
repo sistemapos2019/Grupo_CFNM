49fcd93229c3f5162c21943989fb3d0ba2db5fed
mamasota <quintanillak20@gmail.com>
2019-10-28 16:27:34 -0600

    se modifico, el metodo login

--- entrega_diseño_interfaz/archivos_vue/login.js
+++ entrega_diseño_interfaz/archivos_vue/login.js
@@ -1,13 +1,14 @@
-var vueApp= new Vue({
-    el:"#vueApp",
+var vueLogin= new Vue({
+    el:"#vueLogin",
 
     data:{
           usuario:[],
-
+          parametros:[],
           parametro:{
             login:"",
             pin: ""   
-          }
+          },
+          idmodal:"",
       },
 
       methods:{
@@ -25,45 +26,93 @@ var vueApp= new Vue({
            
             .then(function (response){
                 console.log(response);
-            vueApp.usuario=response.data;
-            vueApp.usuario.forEach(element => {
-                if((element.login== vueApp.parametro.login && element.pin== vueApp.parametro.pin) || (element.login== vueApp.parametro.login && element.clave== vueApp.parametro.pin)){//comparo  el jason si el parametro de login es 1
-                   console.log(vueApp.parametro.login);
-                   console.log(vueApp.parametro.pin); 
-                   alert("AUTENTICACION EXITOSA"); 
-                    if(element.rol=="G"){
-                        console.log("gerente");
-                        //url="dashboard.html";
-                       // window.location.href= url;
-                       //alert("AUTENTICACION EXITOSA");
-    
-                    }else if(element.rol=="M"){
-                        console.log("mesero");
-                       // window.location.href= url;
-                       //alert("AUTENTICACION EXITOSA");
-                    }
-                }else{
-                    alert("Usuario o Contrasena incorrecta");
-                }
-                     
-                
-            });
-            vueApp.parametro={
+                vueLogin.usuario=response.data;
+
+            if (vueLogin.comprobarUsuario(vueLogin.parametro.login)==true && vueLogin.comprobarPassword(vueLogin.parametro.pin)==true) {
+                alert("AUTENTICACION EXITOSA")
+                abrirModal();
+                //url="dashboard.html";
+               // window.location.href= url;
+            } else {
+                alert("USUARIO O CONTRASEÑA INCORRECTA")
+            }
+            vueLogin.parametro={
                 login:"",
                 pin:""
             }
+            
+            
 
             })
            
-            // .then(res => res.json())
-            // .then(data=>{
-                // console.log(data.valor)
-                // console.log("entrando al for");
+           
                 
             
-        }
-        
+        },
 
+        comprobarUsuario:function(login){
+            existe=false;
+   for (let index = 0; index < vueLogin.usuario.length; index++) {
+    if (vueLogin.usuario[index].login==login) {
+        existe=true;
+        break
+    }
+    
 }
+return existe;
+
+        },
+
+        comprobarPassword:function (pin){
+            existe=false;
+
+            for (let index = 0; index < vueLogin.usuario.length; index++) {
+                if (vueLogin.usuario[index].pin==pin || vueLogin.usuario[index].clave==pin) {
+                    existe=true;
+                    break
+                }
+                
+            }
+return existe;
+        },
+
+        mostrarlogin:function(){
+            $('#inicioSecion').modal('show');
+        },
+
+// la funcion loginPantalla no ira aqui si no que en las otras pantalas
+loginporPantalla:function(event){
+
+axios.get('http://localhost:3000/api/parametros')//le paso la url para ir a traer los datos de la api
+           
+.then(function (response){
+    console.log(response);
+    vueLogin.parametros=response.data;
+
+if (vueLogin.parametros[11].valor==1 ) {
+
+//     let boton  = event.target.data("idModal");
+// vueLogin.idmodal = boton;
+
+//alert(event.currentTarget.tagName)
+vueLogin.mostrarlogin();
+//autenticacion();//esta funcion va en el boton login del modal (o no se como le has puesto al boton para iniciar sesion XD) que abris en la linea 100 del codigo
+} else {
+    
+}
+
+})
+
+},
+
+abrirModal:function(){
+$('#inicioSeccion').modal("hide");
+// $(vueLogin.idmodal).modal("show")
+windows.location.href=vueLogin.idmodal;
+}
+
+
+},
+
 
 });
